82845686001628ca6ee8929e16d68e0d05a4d275
jmateos-jdev <joaquin.mateos@jdev.com.ar>
2024-05-30 14:33:23 -0300

    screenshot and video

--- vite-project/cypress.config.js
+++ vite-project/cypress.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-  },
-});
+    }
+  }
+})
